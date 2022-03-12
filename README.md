802174ee78e27d7f006b700d2e9a5244e2f286bc
OriKedar <orikedarr@gmail.com>
2022-03-12 18:20:51 +0200

    create a single file that working

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Run the file
+Welocme the my first eco server.
+
+to run this ecoserver you will need run 
+```npm install```
+and then run the file app with ```node app.ts```
+
--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,16 @@ const app = express();
 const port = 8080;
 const path = require('path');
 
-// app.get('/yoav', (req, res) => {
-//     res.send('Hello Yoav!');
-//     console.log('Yoav is calling');
-// })
+app.get('/yoav', (req, res) => {
+    res.send('Hello Yoav!');
+    console.log('Yoav is calling');
+})
+
+app.get('/:id', (req, res) => {
+    res.send(req.params.id);
+    console.log(req.params.id);
+})
 
 app.listen( port, () => {
-    console.log(`Exmple app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 })
