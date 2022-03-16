9dc3bf94df06e60e35c3298aeb76729e0e42a6e9
OriKedar <orikedarr@gmail.com>
2022-03-16 10:46:24 +0200

    working server

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,14 @@
-// app.get('/', (req, res) => {
-//     res.send('Welcome to my echo server');
-// })
+import express, {Request, Response } from "express";
 
-/*
-run from a single file, task I
-*/
-const express = require('express');
+
 const app = express();
 const port = 8080;
-const path = require('path');
-import * as echoHandler from "./handlers/echoHandler/echoHandler"
 
 
-
-// app.get('/:Id', (req, res) => {
-//     res.send(`Welcome to my echo server ${req.params.Id}\n`)
-// })
-app.get("/", echoHandler.hello);
+app.get("/*", (_req:Request, res:Response) => {
+    res.send("some other")
+})
 
 app.listen( port, () => {
     console.log(`Exmple app listening on port ${port}`);
 })
-
-
-// import app from "./app"
-
-
-// const server = app.listen(app.get("port"), () => {
-//     console.log(
-//         "  App is running at http://localhost:%d in %s mode",
-//         app.get("port"),
-//     );
-//     console.log("  Press CTRL-C to stop\n");
-// });
-
-// export default server;
